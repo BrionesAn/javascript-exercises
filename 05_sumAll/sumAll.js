34718924c3f720e4844078ec28713521996a0a3f
BrionesAn <122240368+BrionesAn@users.noreply.github.com>
2024-08-24 18:31:02 -0700

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(operand1, operand2) {
+    let sumAll = 0;
+    if (operand1 > 0 && operand2 > 0 && Number.isInteger(operand1) && Number.isInteger(operand2)) {
+        if (operand2 > operand1) {
+            for ( ; operand1 <= operand2; operand1++) {
+                sumAll += operand1;
+            }
+        } else if (operand1 > operand2) {
+            for ( ; operand2 <= operand1; operand2++) {
+                sumAll += operand2;
+            }
+        } else {
+            return operand1;
+        }
+    } else {
+        return "ERROR";
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
