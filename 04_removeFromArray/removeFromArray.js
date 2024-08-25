e75cd9224a4f5caff556c44fe332f514a6dd628b
BrionesAn <122240368+BrionesAn@users.noreply.github.com>
2024-08-24 18:22:16 -0700

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...restOfArguments) {
+/* 
+    Iterate array searching for specified arguments
+    splice the array if found an argument
+    return array
+*/
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j <restOfArguments.length; j++) {
+            if (array[i] === restOfArguments[j]) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
+
 };
 
 // Do not edit below this line
