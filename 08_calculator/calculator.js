a83205b9e84054f3234bc72fee6a4ff504de2be5
BrionesAn <122240368+BrionesAn@users.noreply.github.com>
2024-09-02 13:21:01 -0700

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(operand1, operand2) {
+  return operand1 + operand2;
 };
 
-const subtract = function() {
-	
+const subtract = function(operand1, operand2) {
+	return operand1 - operand2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce ( (total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(operand1, operand2) {
+	return Math.pow(operand1, operand2);
 };
 
-const factorial = function() {
-	
+const factorial = function(operand) {
+  let total = 1;
+	for (let i = 1; i <= operand; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
