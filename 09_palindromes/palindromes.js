2cfed5c419e9da45838f821d956c9a6bd1db2926
BrionesAn <122240368+BrionesAn@users.noreply.github.com>
2024-09-02 15:21:43 -0700

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let newWord = word.toLowerCase();
+    newWord = newWord.replace(/[\s,!?.]/g, ""); // Use a regex to remove spaces and punctuation
+    let j = newWord.length - 1;
 
+    for (let i = 0; i < newWord.length / 2; i++) {
+        console.log(newWord[i] + " " + newWord[j]);
+        if (newWord[i] !== newWord[j]) { // Use strict equality
+            return false;
+        }
+        j--;
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
