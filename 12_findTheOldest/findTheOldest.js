28b37a68f1517e1eab5a92458d589fde048b79b5
BrionesAn <122240368+BrionesAn@users.noreply.github.com>
2024-09-03 16:48:42 -0700

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+// Map over array and create a new array of ages
+// Loop over array and return index of oldest person
+
+let ages = people.map( (person) => {
+    if (typeof person.yearOfDeath === 'undefined') {
+        return (new Date().getFullYear() - person.yearOfBirth);
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+});
+
+let oldestAge = 0;
+let oldestIndex = 0;
+for (let i = 0; i < ages.length; i++) {
+    if (ages[i] > oldestAge) {
+        oldestAge = ages[i];
+        oldestIndex = i;
+    }
+}
+
+return people[oldestIndex];
 
 };
 
